1127cbac115ea6f405a91a9b0b13d8e8bba42a51
Mahmoud Hammad <mahmoudhammad3009@gmail.com>
2022-08-24 16:34:25 +0300

    Fix connection

--- src/database/config/connections.js
+++ src/database/config/connections.js
@@ -1,13 +1,34 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const { DATABASE_URL } = process.env;
+const {
+  DEV_DB_URL, TEST_DB_URL, DATABASE_URL, NODE_ENV,
+} = process.env;
 
-if (!DATABASE_URL) throw new Error('database NOT Found');
+let DB_URL = '';
+
+switch (NODE_ENV) {
+  case 'production':
+    DB_URL = DATABASE_URL;
+    break;
+  case 'development':
+    DB_URL = DEV_DB_URL;
+    break;
+  case 'test':
+    DB_URL = TEST_DB_URL;
+    break;
+  default:
+    throw new Error('No Database Found');
+}
 
 const connection = new Pool({
-  connectionString: DATABASE_URL,
-  ssl: new URL(DATABASE_URL).hostname !== 'localhost',
+  connectionString: DB_URL,
+  ssl:
+    NODE_ENV === 'production'
+      ? {
+        rejectUnauthorized: false,
+      }
+      : false,
 });
 
 module.exports = connection;
